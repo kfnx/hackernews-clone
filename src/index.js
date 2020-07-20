525ccb37f29370475702732c0aa2edde244d47f3
kafinsalim <kafinsalim@gmail.com>
2020-07-20 22:41:29 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const { GraphQLServer } = require("graphql-yoga");
+
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+  {
+    id: "link-1",
+    url: "www.howtographql1.com",
+    description: "Fullstack tutorial for GraphQLL",
+  },
+  {
+    id: "link-2",
+    url: "www.howtographql2222.com",
+    description: "Fullstack tutorial for GraphQLLLLL",
+  },
+];
+
+let idCount = links.length;
+
+const resolvers = {
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    // 2
+    feed: () => links,
+  },
+  Mutation: {
+    // 2
+    post: (parent, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+  }, //   },
+};
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: "./src/schema.graphql",
+  resolvers,
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
