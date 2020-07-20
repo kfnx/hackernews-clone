95c9725f8c22757b77ba18f2fda19f639131022c
kafinsalim <kafinsalim@gmail.com>
2020-07-20 23:10:07 +0700

    add CRUD mutation to links

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ const resolvers = {
     info: () => `This is the API of a Hackernews Clone`,
     // 2
     feed: () => links,
+    link: (parent, args) => links[args.id],
   },
   Mutation: {
     // 2
@@ -37,7 +38,28 @@ const resolvers = {
       links.push(link);
       return link;
     },
-  }, //   },
+    updateLink: (parent, args) => {
+      const link = {
+        id: `link-${args.id}`,
+        url: args.url ? args.url : links[args.id].url,
+        description: args.description
+          ? args.description
+          : links[args.id].description,
+      };
+      links[args.id] = link;
+      return link;
+    },
+    deleteLink: (parent, args) => {
+      if (links[args.id]) {
+        links.splice(args.id, 1);
+        return links;
+      } else {
+        const error = Error(`link with id: ${args.id} doesn't exist!`);
+        Error.captureStackTrace(error, this.get);
+        throw error;
+      }
+    },
+  },
 };
 
 // 3
@@ -45,4 +67,5 @@ const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
 });
+
 server.start(() => console.log(`Server is running on http://localhost:4000`));
